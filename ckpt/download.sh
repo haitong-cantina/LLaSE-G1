1994f0542dd734a2452823a6dfe8e44db1d7bd22
Beau Kang <39934776+Kevin-naticl@users.noreply.github.com>
2025-03-07 00:18:22 +0800

    Update download.sh

--- ckpt/download.sh
+++ ckpt/download.sh
@@ -16,3 +16,9 @@ python download_script.py \
      --repo_id HKUSTAudio/xcodec2 \
      --filename ckpt/epoch=4-step=1400000.ckpt \
      --save_path ./codec_ckpt/epoch=4-step=1400000.ckpt
+
+python download_script.py \
+     --source hf \
+     --repo_id ASLP-lab/LLaSE-G1 \
+     --filename ckpt/model.pt.tar \
+     --save_path ./model.pt.tar
